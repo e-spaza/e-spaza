53086bfc50d66a23a9a64af08c8aa03dfd791c22
“2556575” <2556575@students.wits.ac.za>
2024-04-30 19:19:03 +0200

    Testing ability to search

---
+++ controllers/product.js
@@ -0,0 +1,21 @@
+const productRouter = require('express').Router()
+const Product = require('../models/product');
+//route for retrieving products
+usersRouter.post('/homepage', (req, res) => {
+    // Check if user exists
+    Product.findOne({ product: req.body.product })
+        .then(product => {
+            if (!product) {
+                res.status(400).send('Product does not exist');
+            } else {
+                // Send a response with the product details
+                res.status(200).json(product);
+                
+            }
+        })
+        .catch(err => {
+            res.status(500).send('Server error');
+        });
+});
+
+module.exports = productRouter
--- models/product.js
+++ models/product.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
     id: String,
-    name: String,
+    availability: String,
     price: Number,
-    catagory: String,
-    brand: String,
-    quantity: Number,
-    image: String
+    product: String,
+    quantity: Number
 
 })
 
