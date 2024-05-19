afedd68c2683ae8d8a207f8dee0e3e1825bb628b
“2556575” <2556575@students.wits.ac.za>
2024-05-19 13:25:12 +0200

    Onboarding page is fully operational

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,14 @@
-const usersRouter = require('express').Router()
-const User = require("../models/user")
+const usersRouter = require('express').Router();
+const User = require("../models/user");
 
 usersRouter.post('/signup', (req, res) => {
-    // Check if user already exists
     User.findOne({ email: req.body.email })
         .then(user => {
             if (user) {
                 res.status(400).send('User already exists');
             } else {
-                // If user does not exist, save new user
                 const newUser = new User(req.body);
-                newUser
-                    .save()
+                newUser.save()
                     .then(result => {
                         res.status(201).json(result);
                     })
@@ -25,15 +22,12 @@ usersRouter.post('/signup', (req, res) => {
         });
 });
 
-//route for user login post, send an error if user does not exist
 usersRouter.post('/login', (req, res) => {
-    // Check if user exists
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 res.status(400).send('User does not exist');
             } else {
-                // Send a response with the user details
                 res.status(200).json(user);
             }
         })
@@ -42,34 +36,36 @@ usersRouter.post('/login', (req, res) => {
         });
 });
 
-// Route for removing access
 usersRouter.post('/remove-access', (req, res) => {
-    User.findOneAndDelete({ name: req.body.name})
+    User.findOneAndDelete({ email: req.body.email })
         .then(result => {
-            if (result) {
-                res.status(200).json(result);
-            } else {
-                res.status(400).send('User not found');
+            if (!result) {
+                return res.status(400).send('User not found');
             }
+            res.status(200).json({ message: 'User successfully removed', data: result });
         })
         .catch(err => {
+            console.error(err);
             res.status(500).send('Server error');
         });
 });
 
-// Route for changing role
 usersRouter.post('/change-role', (req, res) => {
-    User.findOneAndUpdate({ name: req.body.name, role: req.body.oldRole }, { role: req.body.newRole })
-        .then(result => {
-            if (result) {
-                res.status(200).json(result);
-            } else {
-                res.status(400).send('User not found');
-            }
-        })
-        .catch(err => {
-            res.status(500).send('Server error');
-        });
+    User.findOneAndUpdate(
+        { email: req.body.email, role: req.body.oldRole },
+        { role: req.body.newRole },
+        { new: true }
+    )
+    .then(result => {
+        if (!result) {
+            return res.status(400).send('User not found');
+        }
+        res.status(200).json(result);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).send('Server error');
+    });
 });
 
-module.exports = usersRouter
+module.exports = usersRouter;
