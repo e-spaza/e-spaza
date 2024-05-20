3a3814032f266e60f62b4b322b8d12ed69783f3c
menzi-shazi <93788906+menzi-shazi@users.noreply.github.com>
2024-05-20 10:11:51 +0000

    updated code coverage

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.js'],
+    coverageReporters: ['lcov', 'text-summary'],
+};
