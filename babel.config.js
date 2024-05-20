7eb05c591d01f075a1ccfe932ab9295cf91baa9f
menzi-shazi <93788906+menzi-shazi@users.noreply.github.com>
2024-05-20 11:26:29 +0000

    babel

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
     presets: [
         [
             '@babel/preset-env',
-            '@babel/preset-typescript',
             {
                 targets: {
                     node: 'current',
